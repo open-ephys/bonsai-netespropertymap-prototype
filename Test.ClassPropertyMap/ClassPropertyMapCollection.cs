1e3ee8a449b54274c25f551cdd21928cd7bf2d59
Aaron Cuevas Lopez <aacuelo@teleco.upv.es>
2025-03-30 15:42:50 +0200

    First working version

---
+++ Test.ClassPropertyMap/ClassPropertyMapCollection.cs
@@ -0,0 +1,21 @@
+﻿using Bonsai.Expressions;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Bonsai.Expressions;
+
+namespace Test.ClassPropertyMap
+{
+    public class ClassPropertyMapCollection : KeyedCollection<string, ClassPropertyMapping>
+    {
+
+        protected override string GetKeyForItem(ClassPropertyMapping item)
+        {
+            return item.Name;
+        }
+    }
+}
---
+++ Test.ClassPropertyMap/ClassPropertyMapping.cs
@@ -0,0 +1,39 @@
+﻿using Bonsai;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Test.ClassPropertyMap
+{
+    /// <summary>
+    /// Represents a dynamic assignment between a selected input source and a property of
+    /// a class that is a property of a workflow element.
+    /// </summary>
+    public sealed class ClassPropertyMapping
+    {
+        /// <summary>
+        /// Initialized a new instance of the <see cref="ClassPropertyMapping"/> class.
+        /// </summary>
+        /// 
+        public ClassPropertyMapping() { }
+
+        public ClassPropertyMapping(string name, string selector)
+        {
+            Name = name;
+            Selector = selector;
+        }
+
+        [TypeConverter(typeof(ClassPropertyMappingNameConverter))]
+        public string Name { get; set; }
+        
+        [XmlAttribute]
+        [DefaultValue("")]
+        [Editor("Bonsai.Design.MultiMemberSelectorEditor, Bonsai.Design", DesignTypes.UITypeEditor)]
+        public string Selector { get; set; }
+
+    }
+}
